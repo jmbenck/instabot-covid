9a2e1b9bc7aedf2f03d0ce339bc8908e6213fdfe
jmbenck <jmbenck@gmail.com>
2021-03-26 03:11:51 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,34 +1,54 @@
 import requests
 import datetime
+import re
 from bs4 import BeautifulSoup
 from PIL import Image, ImageDraw, ImageFont
 from instabot import Bot
+from sys import platform
+
 
 # Start bot and login
 bot = Bot()
-bot.login(username="username", password="password", ask_for_code=True)
+bot.login(username="username", password="password", ask_for_code=True, use_cookie=False)
 
 # Scrape Worldometers website
 url = "https://www.worldometers.info/coronavirus/country/brazil/"
 r = requests.get(url)
 s = BeautifulSoup(r.text, 'html.parser')
-deaths = s.find_all('div', {'class': 'maincounter-number'})
+deaths = s.find_all('div', {'class': "maincounter-number"})
 deaths = deaths[1].find('span').text
-print(deaths)
+updates = s.find('li', class_="news_li").find_all('strong')
+new_cases = (re.search(r"\d+,\d+", updates[0].text).group())
+new_deaths = (re.search(r"\d+,\d+", updates[1].text).group())
 
 # Create Image
 img = Image.new('RGB', (1080, 1080), color=(0, 0, 0))
 d = ImageDraw.Draw(img)
-fnt = ImageFont.truetype('C:\Windows\Fonts\Arial.ttf', 200)
+
+# Check OS and locate font
+if platform == 'win32':
+    fnt = ImageFont.truetype(r"/Windows/Fonts/Arial.ttf", 200)
+else:
+    fnt = ImageFont.truetype(r"/Library/Fonts/Arial.ttf", 200)
+
+# Centralize text
 w, h = d.textsize(deaths, font=fnt)
-d.text(((1080-w)/2, (1080-h)/2), deaths, font=fnt, fill=(255, 255, 255))
+d.text(((1080 - w) / 2, (1080 - h) / 2), deaths, font=fnt, fill=(255, 255, 255))
 
 # Save Image with datetime name
-img.save(f'{datetime.datetime.now():%d-%m-%Y}.jpeg')
+img_name = "{:%d-%m-%Y}.jpeg"
+img_name = img_name.format(datetime.datetime.now())
 
-# Store the name of the image
-img_name = f'{datetime.datetime.now():%d-%m-%Y}.jpeg'
-print(img_name)
+img.save(img_name)
+
 
 # Upload Image
-bot.upload_photo(img_name, caption=f'Dia {datetime.datetime.now():%d/%m/%Y} chegamos ao triste número de {deaths} brasileiros mortos por conta da COVID-19. Fonte:https://www.worldometers.info/coronavirus/country/brazil/')
+caption = '''
+{:%d/%m/%Y} - Chegamos ao triste número de {} brasileiros mortos por conta da COVID-19.
+
+São {} mortos e {} novos casos somente nas últimas 24 horas.
+
+Fonte:https://www.worldometers.info/coronavirus/country/brazil/
+'''
+bot.upload_photo(img_name,
+                 caption=caption.format(datetime.datetime.now(), deaths, new_deaths, new_cases))
